cefecd4856431c8983ebc3123de6d478b371f0b6
navidr08-hub <72671675+navidr08-hub@users.noreply.github.com>
2021-11-10 14:28:23 -0500

    some basic commands

--- index.py
+++ index.py
@@ -1,17 +1,68 @@
 import discord 
+import json
 
-def read_token():
-    with open("token.txt", "r") as f:
-        lines = f.readlines()
-        return lines[0].strip()
+# people_string = '''
+# {
+#     "people: [
+#         {
+#             "name": "John Smith",
+#             "phone": "615-555-7164",
+#             "emails": ["johnsmith@bogusemail.com", "john.smith@work-place.com"],
+#             "has-license": false
+#         },
+#         {
+#             "name": "Jane Doe",
+#             "phone": "560-555-5153",
+#             "emails": null,
+#             "has_license": true
+#         }
+#     ]
+# }
+# '''
+events = {}
+
+def write(dict=events):
+    json_object = json.dumps(dict, indent = 4)
+    with open("events.json", "w") as outfile:
+        outfile.write(json_object)
+
+def enter_event(command):
+    new_event = {
+        "name": command[1],
+        "date": command[2],
+        "time": command[3]
+    }
+
+    # Reading from calendar.json
+    with open("events.json") as c:
+        read_json = json.load(c)
+        events = read_json
+        
+    events[new_event["name"]] = new_event
+
+    write(events)
     
-token = read_token()
-client = discord.Client()
+    
+while True: 
+    command = input("calendar-bot>").split(" ")
 
-@client.event
-async def on_message(message):
-     # print(message.content)
-     if message.content.find("!ping") != -1:
-         await message.channel.send("Pong")
+    if command[0] == "event":
+        enter_event(command)
+    else:
+        print("'%s' not a command. Enter --commands to see a list of valid commands" % command[0])
+
+# def read_token():
+#     with open("token.txt", "r") as f:
+#         lines = f.readlines()
+#         return lines[0].strip()
+    
+# token = read_token()
+# client = discord.Client()
+
+# @client.event
+# async def on_message(message):
+#      # print(message.content)
+#      if message.content.find("!ping") != -1:
+#          await message.channel.send("Pong")
      
-client.run(token)
+# client.run(token)
