e483e79ae604677c6ba325f8b99c635d0d992d0a
navidr08-hub <72671675+navidr08-hub@users.noreply.github.com>
2021-11-12 07:15:32 -0500

    Its working

--- index.py
+++ index.py
@@ -100,7 +100,7 @@ def add_change(msg):
     # Reading from events.json
     with open("events.json") as r:
         file = json.load(r) # type = dictionary
-        print(file)
+
         # Event to be added
         new_event = {
             "name": msg[1],
@@ -110,78 +110,125 @@ def add_change(msg):
         }
         
         if msg[0] == "$change" and file.get(msg[1], False) == False:
-                # send("Event does not exist.")
-                print("Event not found.")
-                return False
+            # send("Event does not exist.")
+            print("Event not found.")
+            return False
         elif msg[0] == "$add" and not file.get(msg[1], False) == False:
-                # send("Event already exists. Use the change command to change an exisiting event.")
-                print("Event already exists. Use the 'change' command to change an exisiting event.")
-                return False
+            # send("Event already exists. Use the change command to change an exisiting event.")
+            print("Event already exists. Use the 'change' command to change an exisiting event.")
+            return False
         else:
-                # If change then remove event that is to be changed and just proceed with adding (it again)
-                if msg[0] == "$change":
-                    print("popped ", file.pop(msg[1]))
-                    
-                events = file
-                print(list(events))
+            # If change then remove event that is to be changed and just proceed with adding (it again)
+            if msg[0] == "$change":
+                p = file.get(msg[1])["priority"] - 1    # 2
+                file.pop(msg[1])    # {'name': 'Dude', 'date': '5/23/2022', 'time': '7:00', 'priority': 3}
+                for i in range(p, len(file)):   # 2, 3
+                    file[list(file)[i]]["priority"] -= 1
+                new_event["priority"] -= 1
                 
-                # Checking for time conflict
-                for event in events:
-                    curr = events[event]
-                    if new_event["date"] == curr["date"]:
-                        if new_event["time"] == curr["time"]:
-                            print("Time slot is occupied by an existing event.")
-                            return False
-                
-                # Appending new event
-                events[new_event["name"]] = new_event
-                new_list  = {}
-                
-                # Sorting priorities in each event
-                for event in events:
-                    curr = events[event]
-                    curr_date = curr["date"].split('/')
-                    new_date = new_event["date"].split('/')
-                    # print(curr)
-                    # print(new_event)
-                    if new_date[2] > curr_date[2]: # Year
+            events = file
+            # print(list(events))
+            
+            # Checking for time conflict
+            for event in events:
+                curr = events[event]
+                if new_event["date"] == curr["date"]:
+                    if new_event["time"] == curr["time"]:
+                        print("Time slot is occupied by an existing event. -> ", curr["name"])
+                        return False
+            
+            # Appending new event
+            events[new_event["name"]] = new_event
+            new_list  = {}
+            
+            # Sorting priorities in each event
+            for event in events:
+                curr = events[event]
+                curr_date = curr["date"].split('/')
+                new_date = new_event["date"].split('/')
+                # print(curr)
+                # print(new_event)
+                if new_date[2] > curr_date[2]: # Year
+                    continue
+                elif new_date[2] == curr_date[2]:
+                    # print("year equal")
+                    if  new_date[0] > curr_date[0]: # Month
                         continue
-                    elif new_date[2] == curr_date[2]:
-                        # print("year equal")
-                        if  new_date[0] > curr_date[0]: # Month
+                    elif new_date[0] == curr_date[0]:
+                        # print("month equal")
+                        if new_date[1] > curr_date[1]: # Day
                             continue
-                        elif new_date[0] == curr_date[0]:
-                            # print("month equal")
-                            if new_date[1] > curr_date[1]: # Day
+                        elif new_date[1] == curr_date[1]:
+                            # print("day equal")
+                            curr_time = int(curr["time"].replace(":", ""))
+                            new_time = int(new_event["time"].replace(":", ""))
+                            # print(new_time, curr_time)
+                            if new_time > curr_time:
                                 continue
-                            elif new_date[1] == curr_date[1]:
-                                # print("day equal")
-                                curr_time = int(curr["time"].replace(":", ""))
-                                new_time = int(new_event["time"].replace(":", ""))
-                                # print(new_time, curr_time)
-                                if new_time > curr_time:
-                                    continue
-                    
-                    # Swap priorities
-                    temp = new_event["priority"] # 5
-                    new_event["priority"] = curr["priority"] # 3
-                    curr["priority"] = temp # 5
-                    
-                    # New event for comparison = perviously swapped (curr)
-                    new_event = curr
-                    # print("switch")
                 
-                # Appending to new dictionary based on sorted priorities
-                for num in range(len(events)):
-                    for event in events:
-                        if events[event]["priority"] == num + 1:
-                            new_list[event] = events[event]
-                            
-                write(new_list)
-                                                            
-                return True
+                # Swap priorities
+                temp = new_event["priority"] # 5
+                new_event["priority"] = curr["priority"] # 3
+                curr["priority"] = temp # 5
+                
+                # New event for comparison = perviously swapped (curr)
+                new_event = curr
+                # print("switch")
+            
+            # Appending to new dictionary based on sorted priorities
+            for num in range(len(events)):
+                for event in events:
+                    if events[event]["priority"] == num + 1:
+                        new_list[event] = events[event]
+                        
+            write(new_list)
+                                                        
+            return True
               
-print(add_change(["$change", "Dude", "5/23/2022", "7:00"]))
+print(add_change(["$add", "hm", "4/21/2022", "8:00"]))
+
+    # "Babu": {
+    #     "name": "Babu",
+    #     "date": "5/22/2022",
+    #     "time": "8:00",
+    #     "priority": 1
+    # },
+    # "Hola": {
+    #     "name": "Hola",
+    #     "date": "5/22/2022",
+    #     "time": "9:00",
+    #     "priority": 2
+    # },
+    # "Dude": {
+    #     "name": "Dude",
+    #     "date": "5/23/2022",
+    #     "time": "7:00",
+    #     "priority": 3
+    # },
+    # "Yadu": {
+    #     "name": "Yadu",
+    #     "date": "5/23/2022",
+    #     "time": "9:00",
+    #     "priority": 4
+    # },
+    # "Hey": {
+    #     "name": "Hey",
+    #     "date": "5/23/2022",
+    #     "time": "10:00",
+    #     "priority": 5
+    # },
+    # "Buddy": {
+    #     "name": "Buddy",
+    #     "date": "5/24/2022",
+    #     "time": "3:00",
+    #     "priority": 6
+    # },
+    # "Gay": {
+    #     "name": "Gay",
+    #     "date": "5/24/2022",
+    #     "time": "7:00",
+    #     "priority": 7
+    # }
 
 #yoyo umm well does ur code account for if the event name is larger than 1?
 
